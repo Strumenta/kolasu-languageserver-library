ede1ba34c1c446b8d88f532592a96e4de904d629
Martin Azpillaga Aldalur <martin@minim.tools>
2023-09-29 14:06:07 +0200

    build a jvm library depending on kolasu and lsp4j

---
+++ src/main/kotlin/com/strumenta/kolasu/languageserver/library/KolasuServer.kt
@@ -0,0 +1,154 @@
+package com.strumenta.kolasu.languageserver.library
+
+import com.strumenta.kolasu.model.Named
+import com.strumenta.kolasu.model.Node
+import com.strumenta.kolasu.model.children
+import com.strumenta.kolasu.parsing.KolasuParser
+import com.strumenta.kolasu.parsing.KolasuToken
+import com.strumenta.kolasu.parsing.ParsingResult
+import org.antlr.v4.runtime.Parser
+import org.antlr.v4.runtime.ParserRuleContext
+import org.eclipse.lsp4j.Diagnostic
+import org.eclipse.lsp4j.DidChangeConfigurationParams
+import org.eclipse.lsp4j.DidChangeTextDocumentParams
+import org.eclipse.lsp4j.DidChangeWatchedFilesParams
+import org.eclipse.lsp4j.DidCloseTextDocumentParams
+import org.eclipse.lsp4j.DidOpenTextDocumentParams
+import org.eclipse.lsp4j.DidSaveTextDocumentParams
+import org.eclipse.lsp4j.DocumentSymbol
+import org.eclipse.lsp4j.DocumentSymbolParams
+import org.eclipse.lsp4j.InitializeParams
+import org.eclipse.lsp4j.InitializeResult
+import org.eclipse.lsp4j.Position
+import org.eclipse.lsp4j.PublishDiagnosticsParams
+import org.eclipse.lsp4j.Range
+import org.eclipse.lsp4j.ServerCapabilities
+import org.eclipse.lsp4j.SetTraceParams
+import org.eclipse.lsp4j.SymbolInformation
+import org.eclipse.lsp4j.SymbolKind
+import org.eclipse.lsp4j.TextDocumentSyncKind
+import org.eclipse.lsp4j.TextDocumentSyncOptions
+import org.eclipse.lsp4j.jsonrpc.messages.Either
+import org.eclipse.lsp4j.launch.LSPLauncher
+import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.services.LanguageClientAware
+import org.eclipse.lsp4j.services.LanguageServer
+import org.eclipse.lsp4j.services.TextDocumentService
+import org.eclipse.lsp4j.services.WorkspaceService
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.concurrent.CompletableFuture
+
+class KolasuServer<R : Node, P : Parser, C : ParserRuleContext, T : KolasuToken>(private val parser: KolasuParser<R, P, C, T>, private val includeErrorNodeIssues: Boolean = false) : LanguageServer, TextDocumentService, WorkspaceService, LanguageClientAware {
+
+    private lateinit var client: LanguageClient
+    private val uriToParsingResult: MutableMap<String, ParsingResult<R>> = mutableMapOf()
+
+    fun startCommunication(inputStream: InputStream = System.`in`, outputStream: OutputStream = System.out) {
+        val launcher = LSPLauncher.createServerLauncher(this, inputStream, outputStream)
+        connect(launcher.remoteProxy)
+        launcher.startListening()
+    }
+
+    override fun connect(client: LanguageClient) {
+        this.client = client
+    }
+
+    override fun getTextDocumentService(): TextDocumentService {
+        return this
+    }
+
+    override fun getWorkspaceService(): WorkspaceService {
+        return this
+    }
+
+    override fun initialize(params: InitializeParams?): CompletableFuture<InitializeResult> {
+        val capabilities = ServerCapabilities()
+        capabilities.setTextDocumentSync(TextDocumentSyncOptions().apply { openClose = true; change = TextDocumentSyncKind.Full })
+        capabilities.setDocumentSymbolProvider(true)
+        return CompletableFuture.completedFuture(InitializeResult(capabilities))
+    }
+
+    override fun shutdown(): CompletableFuture<Any> {
+        return CompletableFuture.completedFuture(null)
+    }
+
+    override fun exit() {
+    }
+
+    override fun didOpen(params: DidOpenTextDocumentParams?) {
+        params?.apply {
+            parseAndPublishDiagnostics(this.textDocument.text, this.textDocument.uri)
+        }
+    }
+
+    override fun didChange(params: DidChangeTextDocumentParams?) {
+        params?.apply {
+            assert(this.contentChanges.size == 1)
+            parseAndPublishDiagnostics(this.contentChanges.first().text, params.textDocument.uri)
+        }
+    }
+
+    private fun parseAndPublishDiagnostics(text: String, uri: String) {
+        val parsingResult = parser.parse(text)
+        uriToParsingResult[uri] = parsingResult
+
+        val diagnostics = ArrayList<Diagnostic>()
+        parsingResult.issues.filter { issue ->
+            includeErrorNodeIssues || !issue.message.startsWith("Error node found")
+        }.forEach { issue ->
+            issue.position?.let {
+                diagnostics.add(Diagnostic(toLSPRange(it), issue.message))
+            }
+        }
+        client.publishDiagnostics(PublishDiagnosticsParams(uri, diagnostics))
+    }
+
+    override fun didClose(params: DidCloseTextDocumentParams?) {
+    }
+
+    override fun didSave(params: DidSaveTextDocumentParams?) {
+    }
+
+    override fun didChangeConfiguration(params: DidChangeConfigurationParams?) {
+    }
+
+    override fun didChangeWatchedFiles(params: DidChangeWatchedFilesParams?) {
+    }
+
+    override fun setTrace(params: SetTraceParams?) {
+    }
+
+    override fun documentSymbol(params: DocumentSymbolParams?): CompletableFuture<MutableList<Either<SymbolInformation, DocumentSymbol>>> {
+        val uri = params?.textDocument?.uri ?: return CompletableFuture.completedFuture(null)
+        val parsingResult = uriToParsingResult[uri] ?: return CompletableFuture.completedFuture(null)
+        val root = parsingResult.root ?: return CompletableFuture.completedFuture(null)
+        val rootPosition = root.position ?: return CompletableFuture.completedFuture(null)
+
+        val namedTree = DocumentSymbol("Named tree", SymbolKind.Variable, toLSPRange(rootPosition), toLSPRange(rootPosition), "", mutableListOf())
+        appendNamedChildren(root, namedTree)
+
+        return CompletableFuture.completedFuture(mutableListOf(Either.forRight(namedTree)))
+    }
+
+    private fun appendNamedChildren(node: Node, parent: DocumentSymbol) {
+        var nextParent = parent
+        if (node is Named) {
+            node.position?.let {
+                val range = toLSPRange(it)
+                val symbol = DocumentSymbol(node.name, SymbolKind.Variable, range, range, "", mutableListOf())
+                parent.children.add(symbol)
+                nextParent = symbol
+            }
+        }
+        node.children.forEach { child ->
+            appendNamedChildren(child, nextParent)
+        }
+    }
+
+    private fun toLSPRange(kolasuRange: com.strumenta.kolasu.model.Position): Range {
+        val start = Position(kolasuRange.start.line - 1, kolasuRange.start.column)
+        val end = Position(kolasuRange.end.line - 1, kolasuRange.end.column)
+        return Range(start, end)
+    }
+}
