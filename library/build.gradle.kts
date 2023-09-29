ac2823547f30635120792502e13f22ad6e3af651
Martin Azpillaga Aldalur <martin@minim.tools>
2023-09-29 16:52:18 +0200

    enable ktlint in the library project

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,21 +1,22 @@
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.8.22"
-	id("maven-publish")
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
+    id("maven-publish")
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("com.strumenta.kolasu:kolasu-core:1.5.31")
-	implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.21.1")
+    implementation("com.strumenta.kolasu:kolasu-core:1.5.31")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.21.1")
 }
 
 group = "com.strumenta"
 version = "0.0.0"
 
 publishing {
-	repositories {mavenLocal()}
-	publications {create<MavenPublication>("mavenJava"){from(components["java"])}}
+    repositories { mavenLocal() }
+    publications { create<MavenPublication>("mavenJava") { from(components["java"]) } }
 }
