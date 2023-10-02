c327e9fa4550c32c97464fd53da99764092619d8
Martin Azpillaga Aldalur <martin@minim.tools>
2023-10-02 15:03:26 +0200

    add createExtensionFolder task to plugin

--- plugin/src/main/java/com/strumenta/kolasu/languageserver/KolasuServerPlugin.java
+++ plugin/src/main/java/com/strumenta/kolasu/languageserver/KolasuServerPlugin.java
@@ -1,6 +1,10 @@
 package com.strumenta.kolasu.languageserver;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
@@ -13,6 +17,7 @@ public class KolasuServerPlugin implements Plugin<Project>
 	public void apply(Project project)
 	{
 		addInstallDependenciesTask(project);
+		addCreateExtensionFolderTask(project);
 	}
 
 	private void addInstallDependenciesTask(Project project)
@@ -22,18 +27,38 @@ public class KolasuServerPlugin implements Plugin<Project>
 		installDependenciesTask.setDescription("Install node dependencies under build/node_modules");
 
 		var actions = new ArrayList<Action<? super Task>>();
+		actions.add(task -> {
+			var npm = new ProcessBuilder("npm", "install", "--prefix", "build", "vscode-languageclient");
+			try {
+				npm.start();
+			} catch (Exception exception) {
+				System.err.println(exception.getMessage());
+			}
+		});
+		installDependenciesTask.setActions(actions);
+	}
+
+	private void addCreateExtensionFolderTask(Project project)
+	{
+		var createExtensionFolderTask = project.getTasks().create("createExtensionFolder");
+		createExtensionFolderTask.setGroup("kolasuServer");
+		createExtensionFolderTask.setDescription("Create language server extension folder for vscode under build/extension");
+
+		var actions = new ArrayList<Action<? super Task>>();
 		actions.add(task ->
 		{
-			var npm = new ProcessBuilder("npm", "install", "--prefix", "build", "vscode-languageclient");
 			try
 			{
-				npm.start();
+				var languageName = project.getName().replace("kolasu-", "").replace("-languageserver", "");
+				Files.createDirectories(Paths.get("build", "extension"));
+				var lines = new ArrayList<String>();
+				Files.write(Paths.get("build", "extension", "package.json"), List.of(languageName), StandardOpenOption.CREATE);
 			}
 			catch (Exception exception)
 			{
 				System.err.println(exception.getMessage());
 			}
 		});
-		installDependenciesTask.setActions(actions);
+		createExtensionFolderTask.setActions(actions);
 	}
 }
