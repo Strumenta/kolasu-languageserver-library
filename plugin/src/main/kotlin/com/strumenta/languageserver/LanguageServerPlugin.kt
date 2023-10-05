0a73b175071e1fc5852c9fddfe1fa1cff27fe599
Martin Azpillaga Aldalur <martin@minim.tools>
2023-10-05 18:45:39 +0200

    enable the single line language server experience

--- plugin/src/main/kotlin/com/strumenta/languageserver/LanguageServerPlugin.kt
+++ plugin/src/main/kotlin/com/strumenta/languageserver/LanguageServerPlugin.kt
@@ -1,6 +1,7 @@
 package com.strumenta.languageserver
 
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -41,101 +42,103 @@ class LanguageServerPlugin : Plugin<Project?> {
         project.dependencies.add("implementation", "org.eclipse.lsp4j:org.eclipse.lsp4j:0.21.1")
         project.dependencies.add("testImplementation", "org.jetbrains.kotlin:kotlin-test-junit:1.8.22")
 
-        addCreateVscodeExtensionTask(project)
-        addLaunchVscodeEditorTask(project)
+        val language = project.rootProject.name
+
+        val shadowJar = project.tasks.getByName("shadowJar") as ShadowJar
+        shadowJar.manifest.attributes["Main-Class"] = "com.strumenta.$language.languageserver.MainKt"
+        shadowJar.archiveFileName.set("$language.jar")
 
         project.extensions.add("languageServer", LanguageServerExtension::class.java)
         extension = project.extensions.getByType(LanguageServerExtension::class.java)
+        extension.language = language
+        extension.fileExtensions = mutableListOf(".$language")
+        extension.editor = "code"
+        extension.shadowJarName = language
+        extension.examples = project.rootDir.absolutePath + "/examples"
+
+        addCreateVscodeExtensionTask(project)
+        addLaunchVscodeEditorTask(project)
     }
 
     private fun addLaunchVscodeEditorTask(project: Project) {
         project.tasks.create("launchVscodeEditor").apply {
             this.group = "language server"
             this.description = "Launch the configured vscode editor with the language server installed (defaults to code)"
-            this.actions = listOf(
-                Action { _ ->
-                    try {
-                        ProcessBuilder(extension.editor, "--extensionDevelopmentPath", "${project.projectDir}/build/extension", "${project.projectDir}/${extension.examples}").start().waitFor()
-                    } catch (exception: Exception) {
-                        System.err.println(exception.message)
-                    }
-                }
-            )
+            this.actions = listOf(Action { _ -> try { launchVscodeEditor(project) } catch (exception: Exception) { System.err.println(exception.message) } })
             this.dependsOn(project.tasks.getByName("createVscodeExtension"))
         }
+    }
+
+    private fun launchVscodeEditor(project: Project) {
+        ProcessBuilder(extension.editor, "--extensionDevelopmentPath", "${project.projectDir}/build/extension", extension.examples).directory(project.projectDir).start().waitFor()
     }
 
     private fun addCreateVscodeExtensionTask(project: Project) {
         project.tasks.create("createVscodeExtension").apply {
             this.group = "language server"
             this.description = "Create language server extension folder for vscode under build/extension"
-            this.actions = listOf(
-                Action { _ ->
-                    try {
-                        val name = extension.language
-                        Files.createDirectories(Paths.get("build", "extension"))
-                        var grammars = ""
-                        if (Files.exists(Paths.get("src", "main", "resources", extension.textmateGrammar))) {
-                            grammars = """
-                            ,
-                            "grammars":
-                            [
-                                {"language": "$name", "scopeName": "${extension.textmateGrammarScope}", "path": "./grammar.tmLanguage"}
-                            ]
-                            """.trimIndent()
-                            Files.copy(Paths.get("src", "main", "resources", extension.textmateGrammar), Paths.get("build", "extension", "grammar.tmLanguage"), StandardCopyOption.REPLACE_EXISTING)
-                        }
-                        Files.writeString(
-                            Paths.get("build", "extension", "package.json"),
-                            """
-                            {
-                                "name": "${name.lowercase(Locale.getDefault())}",
-                                "version": "0.0.0",
-                                "contributes":
-                                {
-                                    "languages":
-                                    [
-                                        {"id": "$name", "extensions": ["${extension.fileExtensions.joinToString("\", \"")}"]}
-                                    ]$grammars
-                                },
-                                "engines": {"vscode": "^1.52.0"},
-                                "activationEvents": ["onLanguage:$name"],
-                                "main": "client.js"
-                            }
-                            """.trimIndent()
-                        )
-                        Files.writeString(
-                            Paths.get("build", "extension", "client.js"),
-                            """
-                            let {LanguageClient} = require("../node_modules/vscode-languageclient/node");
-    
-                            async function activate (context)
-                            {
-                                let productionServer = {run: {command: "java", args: ["-jar", context.asAbsolutePath("server.jar")]}};
-    
-                                let languageClient = new LanguageClient("$name", "$name language server", productionServer, {documentSelector: ["$name"]});
-                                await languageClient.start();
-    
-                                context.subscriptions.push(languageClient);
-                            }
-    
-                            module.exports = {activate};
-                            """.trimIndent()
-                        )
-
-                        if (extension.shadowJarName == "") {
-                            extension.shadowJarName = project.name
-                        }
-                        Files.copy(Paths.get("build", "libs", extension.shadowJarName + ".jar"), Paths.get("build", "extension", "server.jar"), StandardCopyOption.REPLACE_EXISTING)
-
-                        ProcessBuilder("npm", "install", "--prefix", "build", "vscode-languageclient").start().waitFor()
-                        ProcessBuilder("npx", "esbuild", "build/extension/client.js", "--bundle", "--external:vscode", "--format=cjs", "--platform=node", "--outfile=build/extension/client.js", "--allow-overwrite").start().waitFor()
-                    } catch (exception: Exception) {
-                        System.err.println(exception.message)
-                    }
-                }
-            )
+            this.actions = listOf(Action { _ -> try { createVscodeExtension(project) } catch (exception: Exception) { System.err.println(exception.message) } })
             this.dependsOn.add(project.tasks.getByName("shadowJar"))
         }
+    }
+
+    private fun createVscodeExtension(project: Project) {
+        val root = project.projectDir.toString()
+        val name = extension.language
+        Files.createDirectories(Paths.get(root, "build", "extension"))
+        var grammars = ""
+        if (Files.exists(Paths.get(root, "src", "main", "resources", extension.textmateGrammar))) {
+            grammars = """
+            ,
+            "grammars":
+            [
+                {"language": "$name", "scopeName": "${extension.textmateGrammarScope}", "path": "./grammar.tmLanguage"}
+            ]
+            """.trimIndent()
+            Files.copy(Paths.get(root, "src", "main", "resources", extension.textmateGrammar), Paths.get(root, "build", "extension", "grammar.tmLanguage"), StandardCopyOption.REPLACE_EXISTING)
+        }
+        Files.writeString(
+            Paths.get(root, "build", "extension", "package.json"),
+            """
+        {
+            "name": "${name.lowercase(Locale.getDefault())}",
+            "version": "0.0.0",
+            "contributes":
+            {
+                "languages":
+                [
+                    {"id": "$name", "extensions": ["${extension.fileExtensions.joinToString("\", \"")}"]}
+                ]$grammars
+            },
+            "engines": {"vscode": "^1.52.0"},
+            "activationEvents": ["onLanguage:$name"],
+            "main": "client.js"
+        }
+            """.trimIndent()
+        )
+
+        Files.writeString(
+            Paths.get(root, "build", "extension", "client.js"),
+            """
+        let {LanguageClient} = require("../node_modules/vscode-languageclient/node");
+
+        async function activate (context)
+        {
+            let productionServer = {run: {command: "java", args: ["-jar", context.asAbsolutePath("server.jar")]}};
+
+            let languageClient = new LanguageClient("$name", "$name language server", productionServer, {documentSelector: ["$name"]});
+            await languageClient.start();
+
+            context.subscriptions.push(languageClient);
+        }
+
+        module.exports = {activate};
+            """.trimIndent()
+        )
+
+        Files.copy(Paths.get(root, "build", "libs", extension.shadowJarName + ".jar"), Paths.get(root, "build", "extension", "server.jar"), StandardCopyOption.REPLACE_EXISTING)
+
+        ProcessBuilder("npm", "install", "--prefix", "build", "vscode-languageclient").directory(project.projectDir).start().waitFor()
+        ProcessBuilder("npx", "esbuild", "build/extension/client.js", "--bundle", "--external:vscode", "--format=cjs", "--platform=node", "--outfile=build/extension/client.js", "--allow-overwrite").directory(project.projectDir).start().waitFor()
     }
 }
