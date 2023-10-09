b108ba21b0c3e1932b1c33f826b579d536317380
Martin Azpillaga Aldalur <martin@minim.tools>
2023-10-09 22:27:31 +0200

    make all generated files customizable

--- plugin/src/main/kotlin/com/strumenta/languageserver/LanguageServerPlugin.kt
+++ plugin/src/main/kotlin/com/strumenta/languageserver/LanguageServerPlugin.kt
@@ -7,6 +7,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.configurationcache.extensions.capitalized
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
+import org.jetbrains.kotlin.konan.file.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -23,6 +24,9 @@ open class LanguageServerExtension {
     var textmateGrammarScope: String = ""
     var logoPath: Path = Paths.get("")
     var fileIconPath: Path = Paths.get("")
+    var languageClientPath: Path = Paths.get("")
+    var packageDefinitionPath: Path = Paths.get("")
+    var licensePath: Path = Paths.get("")
 }
 
 class LanguageServerPlugin : Plugin<Project?> {
@@ -59,20 +63,163 @@ class LanguageServerPlugin : Plugin<Project?> {
         extension.editor = "code"
         extension.serverJarPath = Paths.get(project.projectDir.toString(), "build", "libs", "$language.jar")
         extension.examplesPath = Paths.get(project.projectDir.toString(), "examples")
+
         extension.logoPath = Paths.get(project.projectDir.toString(), "src", "main", "resources", "logo.png")
         extension.fileIconPath = Paths.get(project.projectDir.toString(), "src", "main", "resources", "fileIcon.png")
+        extension.languageClientPath = Paths.get(project.projectDir.toString(), "src", "main", "resources", "client.js")
+        extension.packageDefinitionPath = Paths.get(project.projectDir.toString(), "src", "main", "resources", "package.json")
+        extension.licensePath = Paths.get(project.projectDir.toString(), "src", "main", "resources", "LICENSE.md")
 
         addCreateVscodeExtensionTask(project)
         addLaunchVscodeEditorTask(project)
+    }
+
+    private fun addLaunchVscodeEditorTask(project: Project) {
+        project.tasks.create("launchVscodeEditor").apply {
+            group = "language server"
+            description = "Launch the configured vscode editor with the language server installed (defaults to code)"
+            actions = listOf(Action { _ -> try { launchVscodeEditor(project) } catch (exception: Exception) { System.err.println(exception.message) } })
+            dependsOn(project.tasks.getByName("createVscodeExtension"))
+        }
+    }
+
+    private fun launchVscodeEditor(project: Project) {
+        ProcessBuilder(extension.editor, "--extensionDevelopmentPath", "${project.projectDir}${File.separator}build${File.separator}vscode", extension.examplesPath.toString()).directory(project.projectDir).start().waitFor()
+    }
+
+    private fun addCreateVscodeExtensionTask(project: Project) {
+        project.tasks.create("createVscodeExtension").apply {
+            group = "language server"
+            description = "Create language server extension folder for vscode under build/vscode"
+            actions = listOf(Action { _ -> try { createVscodeExtension(project) } catch (exception: Exception) { System.err.println(exception.message) } })
+            dependsOn.add(project.tasks.getByName("shadowJar"))
+        }
+    }
+
+    private fun createVscodeExtension(project: Project) {
+        val root = project.projectDir.toString()
+        val language = extension.language
+
+        val shadowJar = project.tasks.getByName("shadowJar") as ShadowJar
+        val entryPoint = shadowJar.manifest.attributes["Main-Class"] as String
+        if (entryPoint == "com.strumenta.$language.languageserver.MainKt") {
+            if (!Files.exists(Paths.get(root, "src", "main", "kotlin", "com", "strumenta", language, "languageserver", "Main.kt"))) {
+                Files.createDirectories(Paths.get(root, "src", "main", "kotlin", "com", "strumenta", language, "languageserver"))
+                Files.writeString(
+                    Paths.get(root, "src", "main", "kotlin", "com", "strumenta", language, "languageserver", "Main.kt"),
+                    """
+                    package com.strumenta.$language.languageserver
+        
+                    import com.strumenta.$language.parser.${language.capitalized()}KolasuParser
+                    import com.strumenta.kolasu.languageserver.library.KolasuServer
+        
+                    fun main() {
+                        val parser = ${language.capitalized()}KolasuParser()
+                        val server = KolasuServer(parser)
+                        server.startCommunication()
+                    }
+                    """.trimIndent()
+                )
+            }
+        }
+
+        Files.createDirectories(Paths.get(root, "build", "vscode"))
+
+        if (Files.exists(extension.packageDefinitionPath)) {
+            Files.copy(extension.packageDefinitionPath, Paths.get(root, "build", "vscode", "package.json"), StandardCopyOption.REPLACE_EXISTING)
+        } else {
+            var grammars = ""
+            if (Files.exists(extension.textmateGrammarPath)) {
+                grammars = """
+                ,
+                "grammars":
+                [
+                    {"language": "$language", "scopeName": "${extension.textmateGrammarScope}", "path": "./grammar.tmLanguage"}
+                ]
+                """.trimIndent()
+                Files.copy(extension.textmateGrammarPath, Paths.get(root, "build", "vscode", "grammar.tmLanguage"), StandardCopyOption.REPLACE_EXISTING)
+            }
+
+            var logo = ""
+            if (Files.exists(extension.logoPath)) {
+                logo = """"icon": "logo.png","""
+            }
+
+            var fileIcon = ""
+            if (Files.exists(extension.fileIconPath)) {
+                fileIcon = """, "icon": {"dark": "fileIcon.png", "light": "fileIcon.png"}"""
+            }
+
+            Files.writeString(
+                Paths.get(root, "build", "vscode", "package.json"),
+                """
+                {
+                    "name": "${language.lowercase(Locale.getDefault())}",
+                    "version": "0.0.0",$logo
+                    "publisher": "strumenta",
+                    "contributes":
+                    {
+                        "languages":
+                        [
+                            {"id": "$language", "extensions": ["${extension.fileExtensions.joinToString("\", \"")}"]$fileIcon}
+                        ]$grammars
+                    },
+                    "engines": {"vscode": "^1.52.0"},
+                    "activationEvents": ["onLanguage:$language"],
+                    "main": "client.js"
+                }
+                """.trimIndent()
+            )
+        }
+
+        if (Files.exists(extension.languageClientPath)) {
+            Files.copy(extension.languageClientPath, Paths.get(root, "build", "vscode", "client.js"), StandardCopyOption.REPLACE_EXISTING)
+        } else {
+            Files.writeString(
+                Paths.get(root, "build", "vscode", "client.js"),
+                """
+                let {LanguageClient} = require("../node_modules/vscode-languageclient/node");
+        
+                async function activate (context)
+                {
+                    let productionServer = {run: {command: "java", args: ["-jar", context.asAbsolutePath("server.jar")]}};
+        
+                    let languageClient = new LanguageClient("$language", "$language language server", productionServer, {documentSelector: ["$language"]});
+                    await languageClient.start();
+        
+                    context.subscriptions.push(languageClient);
+                }
+        
+                module.exports = {activate};
+                """.trimIndent()
+            )
+        }
+
+        Files.copy(extension.serverJarPath, Paths.get(root, "build", "vscode", "server.jar"), StandardCopyOption.REPLACE_EXISTING)
+
+        ProcessBuilder("npm", "install", "--prefix", "build", "vscode-languageclient").directory(project.projectDir).start().waitFor()
+        ProcessBuilder("npx", "esbuild", "build/vscode/client.js", "--bundle", "--external:vscode", "--format=cjs", "--platform=node", "--outfile=build/vscode/client.js", "--allow-overwrite").directory(project.projectDir).start().waitFor()
+
+        if (Files.exists(extension.logoPath)) {
+            Files.copy(extension.logoPath, Paths.get(root, "build", "vscode", "logo.png"), StandardCopyOption.REPLACE_EXISTING)
+        }
+
+        if (Files.exists(extension.fileIconPath)) {
+            Files.copy(extension.fileIconPath, Paths.get(root, "build", "vscode", "fileIcon.png"), StandardCopyOption.REPLACE_EXISTING)
+        }
+
+        if (Files.exists(extension.licensePath)) {
+            Files.copy(extension.licensePath, Paths.get(root, "build", "vscode", "LICENSE.md"))
+        } else {
+            Files.writeString(Paths.get(root, "build", "vscode", "LICENSE.md"), "Copyright Strumenta SRL")
+        }
+        ProcessBuilder("npx", "vsce", "package", "--allow-missing-repository").directory(Paths.get(project.projectDir.toString(), "build", "vscode").toFile()).redirectErrorStream(true).start().waitFor()
     }
 
     private fun createEntryPoint(project: Project) {
         val root = project.projectDir.toString()
         val language = extension.language
 
-        if (Files.exists(Paths.get(root, "src", "main", "kotlin", "com", "strumenta", language, "languageserver", "Main.kt"))) {
-            return
-        }
         Files.createDirectories(Paths.get(root, "src", "main", "kotlin", "com", "strumenta", language, "languageserver"))
         Files.writeString(
             Paths.get(root, "src", "main", "kotlin", "com", "strumenta", language, "languageserver", "Main.kt"),
@@ -89,122 +236,5 @@ class LanguageServerPlugin : Plugin<Project?> {
             }
             """.trimIndent()
         )
-    }
-
-    private fun addLaunchVscodeEditorTask(project: Project) {
-        project.tasks.create("launchVscodeEditor").apply {
-            this.group = "language server"
-            this.description = "Launch the configured vscode editor with the language server installed (defaults to code)"
-            this.actions = listOf(Action { _ -> try { launchVscodeEditor(project) } catch (exception: Exception) { System.err.println(exception.message) } })
-            this.dependsOn(project.tasks.getByName("createVscodeExtension"))
-        }
-    }
-
-    private fun launchVscodeEditor(project: Project) {
-        ProcessBuilder(extension.editor, "--extensionDevelopmentPath", "${project.projectDir}/build/vscode", extension.examplesPath.toString()).directory(project.projectDir).start().waitFor()
-    }
-
-    private fun addCreateVscodeExtensionTask(project: Project) {
-        project.tasks.create("createVscodeExtension").apply {
-            this.group = "language server"
-            this.description = "Create language server extension folder for vscode under build/vscode"
-            this.actions = listOf(Action { _ -> try { createVscodeExtension(project) } catch (exception: Exception) { System.err.println(exception.message) } })
-            this.dependsOn.add(project.tasks.getByName("shadowJar"))
-        }
-    }
-
-    private fun createVscodeExtension(project: Project) {
-        val root = project.projectDir.toString()
-        val name = extension.language
-
-        val shadowJar = project.tasks.getByName("shadowJar") as ShadowJar
-        val entryPoint = shadowJar.manifest.attributes["Main-Class"] as String
-        if (entryPoint == "com.strumenta.$name.languageserver.MainKt") {
-            if (!Files.exists(Paths.get(root, "src", "main", "kotlin", "com", "strumenta", name, "languageserver", "Main.kt"))) {
-                createEntryPoint(project)
-            }
-        }
-
-        Files.createDirectories(Paths.get(root, "build", "vscode"))
-
-        var grammars = ""
-        if (Files.exists(extension.textmateGrammarPath)) {
-            grammars = """
-            ,
-            "grammars":
-            [
-                {"language": "$name", "scopeName": "${extension.textmateGrammarScope}", "path": "./grammar.tmLanguage"}
-            ]
-            """.trimIndent()
-            Files.copy(extension.textmateGrammarPath, Paths.get(root, "build", "vscode", "grammar.tmLanguage"), StandardCopyOption.REPLACE_EXISTING)
-        }
-
-        var logo = ""
-        if (Files.exists(extension.logoPath)) {
-            logo = """"icon": "logo.png","""
-        }
-
-        var fileIcon = ""
-        if (Files.exists(extension.fileIconPath)) {
-            fileIcon = """, "icon": {"dark": "fileIcon.png", "light": "fileIcon.png"}"""
-        }
-
-        Files.writeString(
-            Paths.get(root, "build", "vscode", "package.json"),
-            """
-        {
-            "name": "${name.lowercase(Locale.getDefault())}",
-            "version": "0.0.0",$logo
-            "publisher": "strumenta",
-            "contributes":
-            {
-                "languages":
-                [
-                    {"id": "$name", "extensions": ["${extension.fileExtensions.joinToString("\", \"")}"]$fileIcon}
-                ]$grammars
-            },
-            "engines": {"vscode": "^1.52.0"},
-            "activationEvents": ["onLanguage:$name"],
-            "main": "client.js"
-        }
-            """.trimIndent()
-        )
-
-        Files.writeString(
-            Paths.get(root, "build", "vscode", "client.js"),
-            """
-        let {LanguageClient} = require("../node_modules/vscode-languageclient/node");
-
-        async function activate (context)
-        {
-            let productionServer = {run: {command: "java", args: ["-jar", context.asAbsolutePath("server.jar")]}};
-
-            let languageClient = new LanguageClient("$name", "$name language server", productionServer, {documentSelector: ["$name"]});
-            await languageClient.start();
-
-            context.subscriptions.push(languageClient);
-        }
-
-        module.exports = {activate};
-            """.trimIndent()
-        )
-
-        Files.copy(extension.serverJarPath, Paths.get(root, "build", "vscode", "server.jar"), StandardCopyOption.REPLACE_EXISTING)
-
-        ProcessBuilder("npm", "install", "--prefix", "build", "vscode-languageclient").directory(project.projectDir).start().waitFor()
-        ProcessBuilder("npx", "esbuild", "build/vscode/client.js", "--bundle", "--external:vscode", "--format=cjs", "--platform=node", "--outfile=build/vscode/client.js", "--allow-overwrite").directory(project.projectDir).start().waitFor()
-
-        if (Files.exists(extension.logoPath)) {
-            Files.copy(extension.logoPath, Paths.get(root, "build", "vscode", "logo.png"), StandardCopyOption.REPLACE_EXISTING)
-        }
-
-        if (Files.exists(extension.fileIconPath)) {
-            Files.copy(extension.fileIconPath, Paths.get(root, "build", "vscode", "fileIcon.png"), StandardCopyOption.REPLACE_EXISTING)
-        }
-
-        if (!Files.exists(Paths.get(project.projectDir.toString(), "build", "vscode", "LICENSE.md"))) {
-            Files.writeString(Paths.get(project.projectDir.toString(), "build", "vscode", "LICENSE.md"), "Copyright Strumenta SRL")
-        }
-        ProcessBuilder("npx", "vsce", "package", "--allow-missing-repository").directory(Paths.get(project.projectDir.toString(), "build", "vscode").toFile()).redirectErrorStream(true).start().waitFor()
     }
 }
