729b8bb36b918787556bc1481e2a73cfa0c97a86
Martin Azpillaga Aldalur <martin@minim.tools>
2023-10-23 08:50:48 +0200

    clean up gradle dependencies and publications

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 repositories {
     mavenCentral()
     maven {
-        name = project.name
         url = uri("https://maven.pkg.github.com/Strumenta/rpg-parser")
         credentials {
             username = (if (extra.has("starlasu.github.user")) extra["starlasu.github.user"] else System.getenv("STRUMENTA_PACKAGES_USER")) as String?
@@ -17,26 +16,20 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.21")
+
     implementation("com.strumenta.kolasu:kolasu-core:1.5.31")
     implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.21.1")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.21")
     implementation("org.apache.lucene:lucene-core:9.8.0")
-    implementation("org.apache.lucene:lucene-queryparser:9.8.0")
-    implementation("org.apache.lucene:lucene-codecs:9.8.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testImplementation("com.strumenta:rpg-parser:2.1.30")
     testImplementation("com.strumenta:rpg-parser-symbol-resolution:2.1.30")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
-
-group = "com.strumenta"
-version = "0.0.0"
 
 publishing {
     repositories {
         maven {
-            name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/Strumenta/kolasu-languageserver-library")
             credentials {
                 username = (if (extra.has("starlasu.github.user")) extra["starlasu.github.user"] else System.getenv("STRUMENTA_PACKAGES_USER")) as String?
@@ -44,9 +37,12 @@ publishing {
             }
         }
     }
-    publications { create<MavenPublication>("mavenJava") { from(components["java"]) } }
-}
-
-tasks.test {
-    useJUnitPlatform()
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.strumenta"
+            artifactId = "language-server"
+            version = "0.0.0"
+            from(components["java"])
+        }
+    }
 }
