8a8522264cf279a83ec82e4ab511a41c2d23a17b
Martin Azpillaga <martin@minim.tools>
2025-05-16 18:44:55 -0300

    set the java process arguments according to debug mode

--- plugin/src/main/kotlin/com/strumenta/kolasu/languageserver/plugin/LanguageServerPlugin.kt
+++ plugin/src/main/kotlin/com/strumenta/kolasu/languageserver/plugin/LanguageServerPlugin.kt
@@ -271,6 +271,10 @@ class LanguageServerPlugin : Plugin<Project?> {
                 StandardCopyOption.REPLACE_EXISTING
             )
         } else {
+            var javaProcessArguments = """["-jar", context.asAbsolutePath("server.jar")]"""
+            if (configuration.debugPort != null) {
+                javaProcessArguments = """["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,quiet=y,address=*:${configuration.debugPort}", "-jar", context.asAbsolutePath("server.jar")]"""
+            }
             Files.writeString(
                 Paths.get(configuration.outputPath.toString(), "client.js"),
                 """
@@ -278,7 +282,7 @@ class LanguageServerPlugin : Plugin<Project?> {
                 
                 async function activate (context)
                 {
-                    let productionServer = {run: {command: "java", args: ["-jar", context.asAbsolutePath("server.jar")]}};
+                    let productionServer = {run: {command: "java", args: $javaProcessArguments}};
                 
                     let languageClient = new LanguageClient("${configuration.language}", "${configuration.language} language server", productionServer, {documentSelector: ["${configuration.language}"]});
                     await languageClient.start();
