5738015c4376050e50a43a42394527079ed0bced
Alessio Stalla <alessiostalla@gmail.com>
2025-06-27 10:40:15 +0200

    Publishing through Maven Central Portal

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,9 +1,8 @@
-import java.net.URI
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    `maven-publish`
+    alias(libs.plugins.mavenPublish)
     signing
 }
 
@@ -25,58 +24,51 @@ java {
 
 val isReleaseVersion = !(project.version as String).endsWith("SNAPSHOT")
 
-publishing {
-    repositories {
-        maven {
-            val releaseRepo = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotRepo = URI("https://oss.sonatype.org/content/repositories/snapshots/")
-            url = if (isReleaseVersion) releaseRepo else snapshotRepo
-            credentials {
-                username = project.properties["ossrhTokenUsername"] as? String
-                password = project.properties["ossrhTokenPassword"] as? String
+mavenPublishing {
+    coordinates(
+        groupId = "com.strumenta.kolasu",
+        artifactId = "language-server",
+        version = project.version as String,
+    )
+
+    pom {
+        name.set("kolasu-language-server")
+        description.set("Language Server Protocol adapter for Kolasu")
+        version = project.version as String
+        packaging = "jar"
+        inceptionYear = "2023"
+        url.set("https://github.com/Strumenta/kolasu-languageserver-library")
+
+        scm {
+            url.set("https://github.com/Strumenta/kolasu-languageserver-library.git")
+            connection.set("scm:git:git:github.com/Strumenta/kolasu-languageserver-library.git")
+            developerConnection.set("scm:git:ssh:github.com/Strumenta/kolasu-languageserver-library.git")
+        }
+
+        licenses {
+            license {
+                name.set("Apache Licenve V2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("repo")
+            }
+        }
+
+        // The developers entry is strictly required by Maven Central
+        developers {
+            developer {
+                id.set("martin-azpillaga")
+                name.set("Martin Azpillaga")
+                email.set("martin.azpillaga@strumenta.com")
+            }
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
             }
         }
     }
-    publications {
-        create<MavenPublication>("language-server-library") {
-            groupId = "com.strumenta.kolasu"
-            artifactId = "language-server"
-            version = project.version as String
-
-            artifact(tasks.getByName("jar"))
-            artifact(tasks.getByName("sourcesJar"))
-            artifact(tasks.getByName("javadocJar"))
-
-            pom {
-                name = "Kolasu language server"
-                description = "Create a language server for parsers created with Kolasu"
-                inceptionYear = "2023"
-                url = "https://github.com/Strumenta/kolasu-languageserver-library"
-                licenses {
-                    license {
-                        name = "Apache License, Version 2.0"
-                        url = "https://opensource.org/license/apache-2-0/"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "martin-azpillaga"
-                        name = "Martin Azpillaga Aldalur"
-                        email = "martin.azpillaga@strumenta.com"
-                    }
-                }
-                scm {
-                    url = "https://github.com/Strumenta/kolasu-languageserver-library.git"
-                    connection = "scm:git:git:github.com/Strumenta/kolasu-languageserver-library.git"
-                    developerConnection = "scm:git:ssh:github.com/Strumenta/kolasu-languageserver-library.git"
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications.getByName("language-server-library"))
+    publishToMavenCentral()
+    signAllPublications()
 }
 
 java {
--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -1,10 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.net.URI
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktlint)
-    `maven-publish`
+    alias(libs.plugins.mavenPublish)
     signing
 }
 
@@ -26,58 +25,46 @@ java {
 
 val isReleaseVersion = !(project.version as String).endsWith("SNAPSHOT")
 
-publishing {
-    repositories {
-        maven {
-            val releaseRepo = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotRepo = URI("https://oss.sonatype.org/content/repositories/snapshots/")
-            url = if (isReleaseVersion) releaseRepo else snapshotRepo
-            credentials {
-                username = project.properties["ossrhTokenUsername"] as? String
-                password = project.properties["ossrhTokenPassword"] as? String
+mavenPublishing {
+    coordinates(
+        groupId = "com.strumenta.kolasu",
+        artifactId = "language-server-testing",
+        version = project.version as String,
+    )
+
+    pom {
+        name.set("kolasu-language-server-testing")
+        description.set("Tools for testing a Language Server Protocol adapter for Kolasu")
+        version = project.version as String
+        packaging = "jar"
+        inceptionYear = "2023"
+        url.set("https://github.com/Strumenta/kolasu-languageserver-library")
+
+        scm {
+            url.set("https://github.com/Strumenta/kolasu-languageserver-library.git")
+            connection.set("scm:git:git:github.com/Strumenta/kolasu-languageserver-library.git")
+            developerConnection.set("scm:git:ssh:github.com/Strumenta/kolasu-languageserver-library.git")
+        }
+
+        licenses {
+            license {
+                name.set("Apache Licenve V2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("repo")
+            }
+        }
+
+        // The developers entry is strictly required by Maven Central
+        developers {
+            developer {
+                id.set("martin-azpillaga")
+                name.set("Martin Azpillaga")
+                email.set("martin.azpillaga@strumenta.com")
             }
         }
     }
-    publications {
-        create<MavenPublication>("language-server-testing") {
-            groupId = "com.strumenta.kolasu"
-            artifactId = "language-server-testing"
-            version = project.version as String
-
-            artifact(tasks.getByName("jar"))
-            artifact(tasks.getByName("sourcesJar"))
-            artifact(tasks.getByName("javadocJar"))
-
-            pom {
-                name = "Kolasu language server testing"
-                description = "Test Kolasu language servers"
-                inceptionYear = "2023"
-                url = "https://github.com/Strumenta/kolasu-languageserver-library"
-                licenses {
-                    license {
-                        name = "Apache License, Version 2.0"
-                        url = "https://opensource.org/license/apache-2-0/"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "martin-azpillaga"
-                        name = "Martin Azpillaga Aldalur"
-                        email = "martin.azpillaga@strumenta.com"
-                    }
-                }
-                scm {
-                    url = "https://github.com/Strumenta/kolasu-languageserver-library.git"
-                    connection = "scm:git:git:github.com/Strumenta/kolasu-languageserver-library.git"
-                    developerConnection = "scm:git:ssh:github.com/Strumenta/kolasu-languageserver-library.git"
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications.getByName("language-server-testing"))
+    publishToMavenCentral()
+    signAllPublications()
 }
 
 java {
